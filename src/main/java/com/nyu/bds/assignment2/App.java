94999d9dc76b948a09e0a390fbd5f598f15f134b
malfusion <judenaveenraj@gmail.com>
2020-03-02 15:49:17 -0500

    Added N-gram generator

--- src/main/java/com/nyu/bds/assignment2/App.java
+++ src/main/java/com/nyu/bds/assignment2/App.java
@@ -1,5 +1,12 @@
 package com.nyu.bds.assignment2;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Properties;
 
 import com.nyu.bds.assignment2.FileOperations;
@@ -8,6 +15,7 @@ import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
 import intoxicant.analytics.coreNlp.StopwordAnnotator;
 /**
  * Hello world!
@@ -16,12 +24,44 @@ import intoxicant.analytics.coreNlp.StopwordAnnotator;
 public class App 
 {
 	
+	public static List<String> getNGrams(List<String> words, List<Integer> sizes){
+		ArrayDeque<String> deque = new ArrayDeque<String>();
+		ArrayList<String> ngrams = new ArrayList<String>();
+		
+		Integer maxSize = Collections.max(sizes);
+		
+		for (String word: words) {
+			if(deque.size() == maxSize) {
+				deque.removeFirst();
+			}
+			deque.addLast(word);
+			Integer currn = 0;
+			StringBuilder sb = new StringBuilder();
+			Iterator<String> iterator = deque.descendingIterator();
+			while (iterator.hasNext()) {
+				String deqWord = iterator.next();
+				
+				if(sb.length() > 0) {
+					sb.insert(0, deqWord + '_');
+				}else {
+					sb.insert(0, deqWord);
+				}
+				
+				currn += 1;
+				if(sizes.contains(currn)) {
+					ngrams.add(sb.toString());
+				}
+			}
+		}
+		
+		return ngrams;
+	}
 	
     public static void main( String[] args )
     {
     	 String content = FileOperations.readFile("/Users/coderpc/Class/BDS/ass2/dataset_3/data/C1/article01.txt");
     	 Properties props = new Properties();
-    	 props.setProperty("annotators", "tokenize,ssplit,stopword,pos,lemma,ner");
+    	 props.setProperty("annotators", "tokenize,ssplit,stopword,pos,lemma,ner,stopword");
          props.setProperty("customAnnotatorClass.stopword", "intoxicant.analytics.coreNlp.StopwordAnnotator");
          props.setProperty("ner.useSUTime", "false");
 
@@ -29,15 +69,21 @@ public class App
 	    // create a document object
         Annotation document = new Annotation(content);
 	    
-	    System.out.println("ASasdasd");
 	    // annotate the document
 	    pipeline.annotate(document);
+	    System.out.println(document.keySet());
 	    
+	    ArrayList<String> words = new ArrayList<String>();
+	    // TODO: Must add NER together here 	    
 	    for (CoreLabel label: document.get(CoreAnnotations.TokensAnnotation.class)) {
-	    	System.out.println(label);
+//	    	System.out.println(label.lemma()+ ' ' +label.ner());
+	    	words.add(label.lemma());
 	    }
+
+//    	ArrayList<String> words = new ArrayList<String>(Arrays.asList("1", "2", "3", "4", "5", "6"));
 	    
-    		       
-	     
+    	ArrayList<Integer> sizes= new ArrayList<Integer>(Arrays.asList(1, 2, 3));
+    	System.out.println(App.getNGrams(words, sizes));
+	    
     }
 }
